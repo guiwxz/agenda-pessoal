49f51125d08d1652c8cfac73ac4c2530e9db0095
Luan Tonin Galvan <luan.galvan@luizalabs.com>
2023-04-08 23:00:08 -0300

    sugestão de mudança

--- src/components/AppWrapper/index.tsx
+++ src/components/AppWrapper/index.tsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import { RequireAuth } from 'react-auth-kit';
+import React from "react";
+import { RequireAuth } from "react-auth-kit";
 
-import { AppHeader } from '../AppHeader/appHeader';
-import { ComposedProviders } from '../../store/composeProviders';
+import { AppHeader } from "../AppHeader/appHeader";
+import { ComposedProviders } from "../../store/composeProviders";
 
 export const AppWrapper: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <RequireAuth loginPath='/login'>
-    <div style={{ display: "flex", flexDirection: 'column' }}>
-      <AppHeader />
-      <div style={{ padding: '10px 12%' }}>
-        <ComposedProviders>
-          {children}
-        </ComposedProviders>
-      </div>
+  <div style={{ display: "flex", flexDirection: "column" }}>
+    <AppHeader />
+    <div style={{ padding: "10px 12%" }}>
+      <ComposedProviders>{children}</ComposedProviders>
     </div>
-  </RequireAuth>
+  </div>
 );
-
-
--- src/containers/home/home.tsx
+++ src/containers/home/home.tsx
@@ -1,177 +1,146 @@
-import React from "react"
-import ModalDialog from "../../components/ModalDialog"
-import { PageHeader } from "../../components/PageHeader"
-import { useContacts } from "../../store/contacts/useContacts"
-import { Photos } from "../../store/people/people.types"
-import { usePeople } from "../../store/people/usePeople"
-import { BorderAnimated, ListTab, TabItem } from "./home.styles"
-import { ModalContent } from "./modalContent"
+import React from "react";
+import ModalDialog from "../../components/ModalDialog";
+import { PageHeader } from "../../components/PageHeader";
+import { useContacts } from "../../store/contacts/useContacts";
+import { usePeople } from "../../store/people/usePeople";
+import { BorderAnimated, ListTab, TabItem, TabItemLabel } from "./home.styles";
+import { ModalContent } from "./modalContent";
 
-import throttle from 'lodash.throttle'
-import api from "../../services/api"
-import { Contacts } from "../../store/contacts/contacts.types"
-import { useAuthUser } from "react-auth-kit"
-import { List } from "../../components/List/list"
-import { ListContent } from "./listContent/listContent"
-
+import throttle from "lodash.throttle";
+import api from "../../services/api";
+import { Contacts } from "../../store/contacts/contacts.types";
+import { List } from "../../components/List/list";
+import { ListContent } from "./listContent/listContent";
 
 export const Home: React.FC<React.PropsWithChildren> = () => {
-
   const { contacts, fetchContacts, favorites, fetchFavorites } = useContacts();
-  const { people, fetchPeople } = usePeople();
 
   const [displayList, setDisplayList] = React.useState<Contacts[]>([]);
 
   const [openModal, setOpenModal] = React.useState<{
-    open: boolean, 
-    values: any,
+    open: boolean;
+    values: any;
     title: string;
-    favorite?: boolean
-  }>({ 
-    open: false, 
+    favorite?: boolean;
+  }>({
+    open: false,
     values: {},
-    title: '' 
+    title: "",
   });
 
-  
-  const [hoverTab, triggerHoverTab] = React.useState<{
-    'favoritos'?: boolean,
-    'todos'?: boolean
-  }>({
-    'favoritos': false,
-    'todos': false
-  })
+  const [activeTab, setActiveTab] = React.useState<"favoritos" | "todos">(
+    "favoritos"
+  );
 
-  const [activeTab, setActiveTab] = React.useState<{
-    'favoritos'?: boolean,
-    'todos'?: boolean
-  }>({
-    'favoritos': true,
-    'todos': false
-  })
+  const fullList = React.useMemo(() => {
+    if (activeTab === "favoritos") return favorites;
 
-  const [photos, setPhotos] = React.useState<Photos[]>([]);
-
-  const fullList = React.useMemo(
-    () => {
-      if (activeTab['favoritos']) 
-        return favorites;
-
-      return [...favorites, ...(contacts.filter((it) => {
+    return [
+      ...favorites,
+      ...contacts.filter((it) => {
         let equal = false;
-        favorites.forEach(fav => {
+        favorites.forEach((fav) => {
           if (fav.id == it.id) {
-            equal = true
-          } 
-        })
-        if (equal) 
-          return false
-        return true
-    }))]},
-    [contacts, favorites, activeTab]
-  )
-
-  
+            equal = true;
+          }
+        });
+        if (equal) return false;
+        return true;
+      }),
+    ];
+  }, [contacts, favorites, activeTab]);
 
   React.useEffect(() => {
     fetchContacts();
     fetchFavorites();
-  }, [])
+  }, []);
 
   React.useEffect(() => {
-    if (activeTab['favoritos']) 
-      setDisplayList(favorites);
-
+    if (activeTab === "favoritos") setDisplayList(favorites);
     else {
-      setDisplayList(fullList)
+      setDisplayList(fullList);
       // setDisplayList([
-      //   ...favorites, 
+      //   ...favorites,
       //   ...(contacts.filter((it) => {
       //     let equal = false;
       //     favorites.forEach(fav => {
       //       if (fav.id == it.id) {
       //         equal = true
-      //       } 
+      //       }
       //     })
-      //     if (equal) 
+      //     if (equal)
       //       return false
       //     return true
       // }))]);
     }
-  }, [favorites, contacts, activeTab])
+  }, [favorites, contacts, activeTab]);
 
-  
   const handleSearch = throttle(
     (term) => {
-      setActiveTab({ todos: true, favoritos: false })
+      setActiveTab("todos");
       if (!term) {
-        setDisplayList(fullList)
+        setDisplayList(fullList);
         return;
       }
       api.searchContacts({ termo: term }).then((res) => {
         if (res) {
-          setDisplayList(res)
+          setDisplayList(res);
         } else {
-          setDisplayList(fullList)
-          
+          setDisplayList(fullList);
         }
-      })
+      });
     },
     500,
     { leading: false }
-  )
-  
+  );
+
   return (
     <div>
-      <PageHeader 
-        onClick={() => setOpenModal({ open: true, values: {}, title: 'Adicionar contato' })}
+      <PageHeader
+        onClick={() =>
+          setOpenModal({ open: true, values: {}, title: "Adicionar contato" })
+        }
         title="Adicionar contato"
         handleSearch={handleSearch}
       />
       <ListTab>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <BorderAnimated active={activeTab['favoritos']} hoverTab={hoverTab['favoritos']}/>
-          <TabItem 
-            active={activeTab['favoritos']}
-            onClick={() => setActiveTab({ 'favoritos': true })} 
-            onMouseEnter={!activeTab['favoritos'] ? (() => triggerHoverTab({'favoritos': true})) : () => {}}
-            onMouseLeave={() => triggerHoverTab({'favoritos': false})}
-          >
-            Favoritos
-          </TabItem>
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <BorderAnimated active={activeTab['todos']} hoverTab={hoverTab['todos']}/>
-          <TabItem 
-            active={activeTab['todos']}
-            onClick={() => setActiveTab({ 'todos': true })}
-            onMouseEnter={!activeTab['todos'] ? (() => triggerHoverTab({'todos': true})) : () => {}}
-            onMouseLeave={() => triggerHoverTab({'todos': false})}
-          >
-            Todos
-          </TabItem>
-        </div>
+        <TabItem
+          active={activeTab === "favoritos"}
+          onClick={() => setActiveTab("favoritos")}
+        >
+          <BorderAnimated className="tab-item-hover" />
+          <TabItemLabel>Favoritos</TabItemLabel>
+        </TabItem>
+
+        <TabItem
+          active={activeTab === "todos"}
+          onClick={() => setActiveTab("todos")}
+        >
+          <BorderAnimated className="tab-item-hover" />
+          <TabItemLabel>Todos</TabItemLabel>
+        </TabItem>
       </ListTab>
-      <List 
+      <List
         data={displayList}
         listItem={(row: Contacts, index) => ({
-          component: <ListContent {...row} index={index} setOpenModal={setOpenModal} />
+          component: (
+            <ListContent {...row} index={index} setOpenModal={setOpenModal} />
+          ),
         })}
       />
       {openModal.open && (
         <ModalDialog
           open={openModal.open}
           title={openModal.title}
-          onClose={() => setOpenModal({ open: false, values: {}, title: '' })}
+          onClose={() => setOpenModal({ open: false, values: {}, title: "" })}
         >
-          <ModalContent 
-            values={openModal.values} 
-            onClose={() => setOpenModal({ open: false, values: {}, title: '' })} 
+          <ModalContent
+            values={openModal.values}
+            onClose={() => setOpenModal({ open: false, values: {}, title: "" })}
             favorite={openModal.favorite}
           />
         </ModalDialog>
       )}
-
     </div>
-  ) 
-}
+  );
+};
